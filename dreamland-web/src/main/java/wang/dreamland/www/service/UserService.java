65639ffb8d3fe98fb7cb74c8272763f91cd6f40c
zhangchi681027 <1229767285@qq.com>
2020-06-06 20:18:10 +0800

    第一次commit

---
+++ dreamland-web/src/main/java/wang/dreamland/www/service/UserService.java
@@ -0,0 +1,14 @@
+package wang.dreamland.www.service;
+
+import wang.dreamland.www.entity.User;
+
+public interface UserService {
+    int regist(User user);
+    User login(String email,String password);
+    User findByEmail(String email);
+    User findByPhone(String phone);
+    User findById(Long id);
+    void deleteByEmail(String email);
+    void update(User user);
+
+}
